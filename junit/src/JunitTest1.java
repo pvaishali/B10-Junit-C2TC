f12a7fc9815babdb770d571a29c5b5b841ebb85e
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-03 12:44:55 +0530

    JUNIT testing

---
+++ junit/src/JunitTest1.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+public class JunitTest1 {
+	@Disabled
+@Test
+void display() {
+	System.out.println("raghul vaishali");
+	System.out.println("raghul ganesh");
+}
+@Test
+void display1() {
+	System.out.println(" vaishali");
+	System.out.println(" ganesh");
+}
+}
