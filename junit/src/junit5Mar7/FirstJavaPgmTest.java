5ef452b8fd72552bede80718045fa6fabd9b34c7
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-07 19:09:23 +0530

    first test prgm

---
+++ junit/src/junit5Mar7/FirstJavaPgmTest.java
@@ -0,0 +1,11 @@
+package junit5Mar7;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+public class FirstJavaPgmTest {
+	@Test
+	public void display() {
+		int A=1;
+		int B=1;
+		assertEquals(A,B);
+	}
+}
